c8f45c7da125a070899cb80e3e8827887c3082ea
it24100622 <it24100622@my.sliit.lk>
2024-08-19 10:14:22 +0530

    Add files via upload

---
+++ Q1/IT24100622Lab4Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner ;
+public class IT24100622Lab4Q1 {
+public static void main(String[]args){
+Scanner scn = new Scanner (System.in);
+System.out.print("Enter a number : ");
+int number = scn.nextInt();
+    if (number>0){
+         System.out.println( " The number is positive ."); }
+   else if (number<0){
+         System.out.println( " The number is negative ."); }
+   else {
+        System.out.println( " The number is Zero."); }
+}
+}
